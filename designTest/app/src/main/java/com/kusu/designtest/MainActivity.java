8eddc1a61200f52737a67620d7a16924d3ed8e66
arc-koushik <koushikcse2015@gmail.com>
2020-01-20 19:12:29 +0530

    Design test

--- designTest/app/src/main/java/com/kusu/designtest/MainActivity.java
+++ designTest/app/src/main/java/com/kusu/designtest/MainActivity.java
@@ -20,15 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.rvList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        MyAdapter adapter=new MyAdapter(this,datelist());
+        MyAdapter adapter = new MyAdapter(this, datelist());
         binding.rvList.setAdapter(adapter);
     }
 
     private List<DataModel> datelist() {
-        List<DataModel> dataModelList=new ArrayList<>();
-        dataModelList.add(new DataModel("Banglore University","Information Technology","05/2015 - 06/2019"));
-        dataModelList.add(new DataModel("IIT Chennai","M. Tech","08/2019 - 06/2020"));
-        dataModelList.add(new DataModel("IIT Mumbai","Phd","07/2020 - 05/2018"));
+        List<DataModel> dataModelList = new ArrayList<>();
+        dataModelList.add(new DataModel("Banglore University", "Information Technology", "05/2015 - 06/2019"));
+        dataModelList.add(new DataModel("IIT Chennai", "M. Tech", "08/2019 - 06/2020"));
+        dataModelList.add(new DataModel("IIT Mumbai", "Phd", "07/2020 - 05/2018"));
 
         return dataModelList;
     }
