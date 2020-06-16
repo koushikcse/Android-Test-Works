b2c039e7bdf451f80f130f2ae3b62bb19441c288
Koushik <koushikcse2015@gmail.com>
2020-06-16 16:36:27 +0530

    package change

--- DeviceWebServer/app/src/main/java/com/example/myapplication/routeServer/ApiUri.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.routeServer
-
-object ApiUri {
-    val START = "/start"
-    val STOP = "/stop"
-    val IS_ACTIVE = "/isActive"
-    val TEST_FILE = "/filetest"
-}
--- DeviceWebServer/app/src/main/java/com/example/myapplication/routeServer/TestModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myapplication.routeServer
-
-data class TestModel(val id: Int = 0, val name: String = "", val uri: String = "")
